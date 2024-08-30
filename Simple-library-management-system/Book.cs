1b25c5471a456db6aaff6376af98b514eb8148de
solesen1992 <saramundal@live.dk>
2024-08-30 09:55:32 +0200

    Updating Books.cs and Library.cs

---
+++ Simple-library-management-system/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_library_management_system
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private string iSBN;
+        private int publishedYear;
+
+        /**
+         * Constructor
+         */
+        public Book(string title, string author, string iSBN, int publishedYear)
+        {
+            this.title = title;
+            this.author = author;
+            this.iSBN = iSBN;
+            this.publishedYear = publishedYear;
+        }
+
+        /**
+         * Getters and setters
+         */
+        public string Title {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author {
+            get { return author; }
+            set { author = value; }
+        
+        }
+        public string ISBN {
+            get { return iSBN; }
+            set { iSBN = value; }
+        }
+
+        public int PublishedYear {
+            get { return publishedYear; }
+            set { publishedYear = value; }
+        }
+
+        /**
+         * To string
+         * Override the ToString method to display the book details in a readable format.
+         */
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, ISBN: {ISBN}, Published Year: {PublishedYear}";
+        }
+    }
+}
---
+++ Simple-library-management-system/Library.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_library_management_system
+{
+    /**
+     * Library class that contains a collection of Book objects.
+     */
+    internal class Library
+    {
+        private List<Book> books;
+
+        /**
+         * Constructor
+         */
+        public Library()
+        {
+            books = new List<Book>;
+        }
+
+        /**
+         * Add a new book to the library
+         */
+        public void AddBookToLibrary(Book book) {
+            books.Add(book);
+            Console.WriteLine("Book added succesfully to the library!");
+        }
+
+        /**
+         * Display all books in the library
+         */
+        public void DisplayBooks() {
+            if (books == null)
+            {
+                Console.WriteLine("There's no books in the library");
+            }
+            else
+            {
+                foreach (Book book in books) {
+                    Console.WriteLine(book.ToString());
+                }
+
+            }
+
+
+        }
+
+        /**
+        * Search for books by title
+        */
+        public void SearchForBooksByTitle(string title) {
+
+        }
+    }
+}
+    
